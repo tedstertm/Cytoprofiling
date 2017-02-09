c376c9642073b6e5dc40c6af2dd33c91fd79ae00
tedstertm <Teddy@Theodoruss-MacBook-Pro.local>
2017-02-09 14:53:21 -0600

    Something
    I know I must have done something here to make it work more better

--- main.R
+++ main.R
@@ -21,7 +21,7 @@ source('CPAClustering.R')
 
 
 #fileHome is where the .db file lives
-fileHome <- '/Users/Teddy/Documents/MM Adhesion/2017-01-19 MM Stacks Adhesion Morphology/DefaultDB.db'
+fileHome <- '/Users/Teddy/Documents/MM Adhesion/2017-01-26 MM Adhesion on Collagen/DefaultDB.db'
 
 
 #source loadDB.R before executing this
@@ -31,14 +31,30 @@ fileHome <- '/Users/Teddy/Documents/MM Adhesion/2017-01-19 MM Stacks Adhesion Mo
 big.Data <- load.db(fileHome, return.variable = MyExpt.Per.Object)
 
 #Sometimes you need to sample several rows instead of running a gajillion features
-small.Data <- big.Data[sample(nrow(big.Data), 1000), ]
-
+small.Data <- big.Data[sample(nrow(big.Data), 2500), ]
 #Get important features with the randomForest function
 
 #This changes depending on what your truth
-small.Data$Truth <- 'Adherent'
-small.Data[small.Data$ImageNumber >= 17,]$Truth <- 'Suspended'
+small.Data$TNF <- 'TNFa-'
+small.Data[small.Data$ImageNumber >= 13,]$TNF <- 'TNFa+'
+small.Data$TNF <- as.factor(small.Data$TNF)
+small.Data$Collagen <- 'Collagen'
+small.Data[small.Data$ImageNumber >= 7,]$Collagen <- 'Fibronectin'
+small.Data[small.Data$ImageNumber >= 13,]$Collagen <- 'Collagen'
+small.Data[small.Data$ImageNumber >= 19,]$Collagen <- 'Fibronectin'
+small.Data$Collagen <- as.factor(small.Data$Collagen)
+small.Data$Adherent <- 'Adherent'
+sus.Imgnum = c(2,3,6,7,10,11,14,15,18,19,22,23)
+small.Data[small.Data$ImageNumber %in% sus.Imgnum,]$Adherent <- 'Suspended'
+small.Data$Adherent <- as.factor(small.Data$Adherent)
+small.Data$Truth <- paste(small.Data$TNF, small.Data$Collagen, small.Data$Adherent, sep="_")
 small.Data$Truth <- as.factor(small.Data$Truth)
+
+#Don't forget to get rid of your half-truths
+small.Data$Adherent = NULL
+small.Data$Collagen = NULL
+small.Data$TNF = NULL
+
 
 #set the randSeed to a couple of different numbers to validate your model, human
 randSeed <- 17
@@ -64,7 +80,32 @@ imp.smallData <- small.Data[, c(imp.Feats)]
 #Use your best guess to figure out how many classes you want, you can change
 #this when you'd like
 
-numClasses <- 3
+numClasses <- 4
 
 imp.smallData$Class <- EMClustBud(imp.smallData, numClasses, return.variable = temp1)
+
+#Plot the things that jay thinks are cool and that are cool. library(Rtsne)
+
+library(Rtsne)
+library(EMCluster)
+library(dplyr)
+numeric.impData <- select_if(small.Data, is.numeric)
+
+sneX <- Rtsne(as.matrix(numeric.impData)) 
+emobj5u <- simple.init(sneX$Y, nclass = 8) 
+clustResults2 <- emcluster(sneX$Y, emobj = emobj5u, pi = NULL, Mu = NULL,
+LTSigma = NULL, lab = NULL, EMC = .EMC, assign.class = TRUE)
+
+pts <- data.frame(sneX$Y)
+
+
+f <- list(family = "Arial", size = 18, color = "#000000") 
+x <- list(title = "x Axis",titlefont = f) 
+y <- list(title = "y Axis",titlefont = f) 
+Class <- clustResults2$class
+
+library(plotly)
+
+plot_ly(mode = 'markers', x=pts$X1, y=pts$X2, color=Class) %>% layout(xaxis =
+x, yaxis = y)
 
